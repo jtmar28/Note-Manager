b0c7aedd02a613e76631b869bd2b9cb5142eb8a0
jtmar28 <justinmartinez0123@gmail.com>
2018-12-10 19:51:49 -0500

    add to list function

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
+const form = document.querySelector('#add');
+const list = document.querySelector('#list');
+const input = document.querySelector('#add-input');
 
+loadAllEventListners();
+
+function loadAllEventListners(){
+    form.addEventListener('submit', addNoteToList);
+}
+
+function addNoteToList(e){
+    const li = document.createElement('li');
+    li.innerHTML = `
+    <p>${input.value}</p>
+    <p><i class="fa fa-pencil-square-o"></i><i class="fa fa-times"></i></p>
+    <input class="edit-note" type="text">
+    `;
+    list.appendChild(li);
+    clearInput();
+    e.preventDefault();
+}
+
+function clearInput(){
+    input.value = '';
+}
 
 
 
