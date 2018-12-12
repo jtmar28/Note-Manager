6374133d883b7a61159baa762c5f38a660bb74a5
jtmar28 <justinmartinez0123@gmail.com>
2018-12-11 20:22:16 -0500

    Delete note from list

--- script.js
+++ script.js
@@ -6,18 +6,30 @@ loadAllEventListners();
 
 function loadAllEventListners(){
     form.addEventListener('submit', addNoteToList);
+    list.addEventListener('click', deleteNoteFromList);
+    list.addEventListener('click', editNoteFromList);
 }
 
 function addNoteToList(e){
-    const li = document.createElement('li');
-    li.innerHTML = `
-    <p>${input.value}</p>
-    <p><i class="fa fa-pencil-square-o"></i><i class="fa fa-times"></i></p>
-    <input class="edit-note" type="text">
-    `;
-    list.appendChild(li);
-    clearInput();
+    if(input.value === ""){
+        alert('please Enter value');
+    }else{
+        const li = document.createElement('li');
+        li.innerHTML = `
+        <p>${input.value}</p>
+        <p><i class="fa fa-pencil-square-o"></i><i class="fa fa-times"></i></p>
+        <input class="edit-note" type="text">
+        `;
+        list.appendChild(li);
+        clearInput();
+    }
     e.preventDefault();
+}
+
+function deleteNoteFromList(e){
+    if(e.target.classList.contains('fa-times')){
+        e.target.parentElement.parentElement.remove();
+    }
 }
 
 function clearInput(){
