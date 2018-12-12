fa2d8c51094a8178e70dedcff013e8c0fba4e921
jtmar28 <justinmartinez0123@gmail.com>
2018-12-11 20:47:43 -0500

    Edit note from list

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ function loadAllEventListners(){
     list.addEventListener('click', editNoteFromList);
 }
 
+//Add note to list
 function addNoteToList(e){
     if(input.value === ""){
         alert('please Enter value');
@@ -25,13 +26,41 @@ function addNoteToList(e){
     }
     e.preventDefault();
 }
-
+//Delete note from list
 function deleteNoteFromList(e){
     if(e.target.classList.contains('fa-times')){
         e.target.parentElement.parentElement.remove();
     }
 }
 
+function editNoteFromList(e){
+    if(e.target.classList.contains('fa-pencil-square-o')){
+        //This empties the note
+        const parentPar = e.target.parentNode;
+        parentPar.style.display = 'none';
+
+        //This is the p element containing the note text value
+        const note = parentPar.previousElementSibling;
+        //This input is initially invisible 
+        const input = parentPar.nextElementSibling;
+        input.style.display = 'block';
+        //The value thats within the p element is equal to the value inside the input
+        input.value = note.textContent;
+        input.addEventListener('keypress', function(e){ 
+            //Key code 13 is the enter key
+            if(e.keyCode === 13){
+                if(input.value !== ""){
+                    note.textContent = input.value;
+                    parentPar.style.display = 'block';
+                    input.style.display = 'none';
+                }else{
+                    let li = input.parentNode;
+                    li.parentNode.removeChild(li);
+                }
+            }
+        });
+    }
+}
 function clearInput(){
     input.value = '';
 }
